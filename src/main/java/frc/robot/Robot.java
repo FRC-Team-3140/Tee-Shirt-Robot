2f960f8c21a7e250eb32bac812de3c23be743afd
ElizabethBC <ElizabethBarCar@gmail.com>
2021-11-30 18:00:49 -0500

    LED's work (kinda)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends TimedRobot {
   private Command autoCommand;
@@ -12,6 +13,7 @@ public class Robot extends TimedRobot {
   private AddressableLED m_led;
   private AddressableLEDBuffer m_ledBuffer;
   private int m_rainbowFirstPixelHue = 0;
+  private Timer led_timer;
 
   @Override
   public void robotInit() {
@@ -19,17 +21,20 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     // PWM port 9
     // Must be a PWM header, not MXP or DIO
-    m_led = new AddressableLED(9);
+    m_led = new AddressableLED(1);
+  
 
     // Reuse buffer
     // Default to a length of 60, start empty output
     // Length is expensive to set, so only set it once, then just update data
-    m_ledBuffer = new AddressableLEDBuffer(60);
+    m_ledBuffer = new AddressableLEDBuffer(32);
     m_led.setLength(m_ledBuffer.getLength());
 
     // Set the data
     m_led.setData(m_ledBuffer);
     m_led.start();
+    led_timer = new Timer();
+    led_timer.start();
     robotContainer = new RobotContainer();
   }
 
@@ -57,6 +62,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     rainbow();
+    // setonoff();
     // Set the LEDs
     m_led.setData(m_ledBuffer);
   }
@@ -75,13 +81,28 @@ public class Robot extends TimedRobot {
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
       // Calculate the hue - hue is easier for rainbows because the color
       // shape is a circle so only one value needs to precess
-      final int hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+      final int hue = (m_rainbowFirstPixelHue + (i * 180)) % 180;
       // Set the value
       m_ledBuffer.setHSV(i, hue, 255, 128);
     }
     // Increase by to make the rainbow "move"
-    m_rainbowFirstPixelHue += 3;
+    // m_rainbowFirstPixelHue += 3;
     // Check bounds
     m_rainbowFirstPixelHue %= 180;
+  }
+
+
+  private void setonoff() {
+    final double current_time=led_timer.getFPGATimestamp();
+    if (current_time%5==0){
+      for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setHSV(i, 0, 255, 255);
+      }
+    }
+    else
+    { for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+       m_ledBuffer.setHSV(i, 0, 255, 0);
+    }
+    }
   }
 }
